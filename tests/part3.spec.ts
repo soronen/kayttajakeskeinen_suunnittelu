d9fcfc215001f3080eb7e97b78667a45c713bccf
tahasafdari <tahahasan.safdari@gmail.com>
2023-04-25 16:34:42 +0300

    tests modified

--- tests/part3.spec.ts
+++ tests/part3.spec.ts
@@ -1,34 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { Eyes, Target } from '@applitools/eyes-playwright';
-
+//import { after } from 'mocha';
 
 const eyes = new Eyes();
 
 // Set your Applitools API key
-eyes.setApiKey("RissvAEsfQ2Y5g2LrXK0yd104aZKaiccVOIkHtRZS18Q4110");
+eyes.setApiKey("youapikey");
 
-/*
-if (process.env.APPLITE_API_KEY) {
-  eyes.setApiKey(process.env.APPLITE_API_KEY);
-} else {
-  console.error('APPLITE_API_KEY environment variable is not defined.');
-}*/
-
-// kymmenen uutiset
-test('kymmenen uutiset', async ({ page }) => {
-  await page.goto('https://areena.yle.fi/tv');
-  await page.getByRole('link', { name: 'TV-opas' }).click();
-  await expect(page.getByText('Kymmenen uutiset')).toBeVisible;
-  await expect(page.getByText(/22.00 Kymmenen uutiset/g)).toBeVisible;
-
-  // Start visual testing with Applitools Eyes
-  await eyes.open(page, 'kymmenen uutiset', 'TV-opas page');
-  await eyes.check('TV-opas page', Target.window().fully());
-  await eyes.close();
-})
 
 // virheellinen sähköpostin muoto
 test('virheellinen sähköpostin muoto', async ({ page }) => {
+ test.setTimeout(120000);
   await page.goto('https://areena.yle.fi/tv')
   await page.waitForLoadState('domcontentloaded')
   await page.getByRole('button', { name: 'Kirjaudu', exact: true }).click()
@@ -74,10 +56,26 @@ test('virheellinen sähköpostin muoto', async ({ page }) => {
   await expect(
     page.frameLocator('internal:role=dialog[name="kirjaudu sisään"i] >> iframe').getByRole('alert')
   ).toHaveText(/Tarkista sähköpostiosoitteen muoto./g)
-})
+
+  await eyes.open(page, 'virheellinen sähköpostin muoto', 'Virheellinen sähköposti');
+  await eyes.check('Virheellinen sähköposti', Target.window().fully());
+  //await eyes.close();
+  
+  /*
+  after(async () => {
+    await eyes.close();
+  });*/
+
+}); 
+//test.afterAll(async() => await eyes.close());
+
+
+
+
 
 // kummeli
 test('kummeli k3j5', async ({ page }) => {
+  test.setTimeout(120000);
   await page.goto('https://areena.yle.fi/tv')
   await page.getByPlaceholder('Hae').click()
   await page.getByPlaceholder('Hae').fill('kummel')
@@ -92,15 +90,28 @@ test('kummeli k3j5', async ({ page }) => {
 
   await expect(page).toHaveTitle(/K3, J5: Kummeli/g)
   await expect(page.locator('time[datetime="2006-01-10T20:00:32+02:00"]')).toHaveText(/10.1.2006/g)
+
+  await eyes.open(page, 'kummeli k3j5', 'Kummeli K3J5');
+  await eyes.check('Kummeli K3J5', Target.window().fully());
+
 })
+
+
 
 // proper channel labels
 test('labeling', async ({ page }) => {
+  test.setTimeout(120000);
   await page.goto('https://areena.yle.fi/tv')
   await page.getByRole('link', { name: 'TV-opas' }).click()
   const logos = await page.$$('.channel-header__logo')
   for (const logo of logos) {
     const ariaLabel = await logo.getAttribute('aria-label')
     expect(ariaLabel).toBeTruthy() // Assert that aria-label contains a non-empty string
+    
   }
-})
+  await eyes.open(page, 'proper channel labels', 'Channel Labels');
+  await eyes.check('Channel Labels', Target.window().fully());
+
+
+
+}) 
